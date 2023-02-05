3971e55e2bd6e2a38cf7eb06b093e6a310cc1111
Rafael Mileris <rafaelmileris15@gmail.com>
2023-02-05 12:51:27 -0300

    Adicionando o evento para devolver os elementos

--- Praticas/Copiando_Elementos/script.js
+++ Praticas/Copiando_Elementos/script.js
@@ -1,7 +1,8 @@
 //Armazenando os items de determinados ID´s em variaveis
 const caixa1 = document.getElementById("caixa1")
 const caixa2 = document.getElementById("caixa2")
-const botao = document.getElementById("btn_copiar")
+const botao_copiar = document.getElementById("btn_copiar")
+const botao_devolver = document.getElementById("btn_devolver")
 
 //Criando um Array com os elementos que estão dentro da classe "items"
 const items = [...document.getElementsByClassName("items")]
@@ -14,10 +15,18 @@ items.map((elementos)=>{
     })
 })
 
-//Realizando um evento ao clicar o botão "copiar"
-botao.addEventListener("click", ()=>{
+//Evento para transferir os elementos da caixa1 para a caixa2
+botao_copiar.addEventListener("click", ()=>{
     const items_selecionados = [...document.querySelectorAll(".destaque")] //Seleciona todos os itens que contem a classe destaque
     items_selecionados.map((elementos)=>{
         caixa2.appendChild(elementos) //appendChild -> Vai pegar o elemento e passar para a div vazia (caixa2)
+    })
+})
+
+//Evento para transferir os elementos da caixa2 para a caixa1
+botao_devolver.addEventListener("click", ()=>{
+    const items_selecionados = [...document.querySelectorAll(".destaque")]
+    items_selecionados.map((elementos)=>{
+        caixa1.appendChild(elementos)
     })
 })
